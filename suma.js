ec8e14457b15c1801a53759267d326c7020b3e6f
cristian felipe almanza wagner <cristianfelipe.almanza@gmail.com>
2015-10-08 20:53:37 -0500

    add JavaScript Logic
    añadí un archivo JavaScript que tiene la lógica de sumar cualquier
    cantidad de números

---
+++ suma.js
@@ -0,0 +1,13 @@
+//declaracion de variables
+var numero1;
+var numero2;
+var resultado;
+
+//asignacion de valores
+numero1 = 65;
+numero2 = 572;
+resultado = numero1 + numero2;
+
+alert("si el primer numero es " + numero1);
+alert("y si el segundo numero es " + numero2);
+alert("entonces el resultado es " + resultado);
